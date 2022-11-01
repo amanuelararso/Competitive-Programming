b1dbb9339aa4796d444596094935c042e688a147
Amanuel Ararso <amanuelararso@gmail.com>
2022-11-02 01:50:16 +0300

    Create fizz_buzz.java

---
+++ fizz_buzz.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3 == 0){
+    if(i%5 == 0)
+        list.add("FizzBuzz");
+    else
+        list.add("Fizz");
+} else if(i%5 == 0)
+    list.add("Buzz");
+else
+    list.add(String.valueOf(i));
+        }
+        return list;
+    }
+}
+
